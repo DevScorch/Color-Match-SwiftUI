805b2cdc2bb960f0a846650fd53621efa2a02ab7
Devscorch <johan.tsas@icloud.com>
2020-06-14 18:33:24 +0200

    Added a Timer

--- Color Match SwiftUI/ContentView.swift
+++ Color Match SwiftUI/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
   @State var rGuess: Double
   @State var gGuess: Double
   @State var bGuess: Double
+  
+  @ObservedObject var timer = TimeCounter()
 
   @State var showAlert = false
 
@@ -27,29 +29,46 @@ struct ContentView: View {
   }
 
   var body: some View {
-    VStack {
-      HStack {
+    NavigationView {
         VStack {
-          Color(red: rTarget, green: gTarget, blue: bTarget)
-          Text("Match this color")
+            HStack {
+                VStack {
+                  Color(red: rTarget, green: gTarget, blue: bTarget)
+                    self.showAlert ? Text("R: \(Int(rTarget * 255.0))"
+                  + "  G: \(Int(gTarget * 255.0))"
+                  + "  B: \(Int(bTarget * 255.0))")
+                    : Text("Match this Color")
+                }
+                VStack {
+                    ZStack { Color(red: rGuess, green: gGuess, blue: bGuess)
+                        Text(String(timer.counter))
+                            .padding(.all, 5)
+                            .background(Color.white)
+                            .mask(Circle())
+                            .foregroundColor(.black)
+                    }
+                  Text("R: \(Int(rGuess * 255.0))"
+                    + "  G: \(Int(gGuess * 255.0))"
+                    + "  B: \(Int(bGuess * 255.0))")
+                }
+              }
+              Button(action: {
+                self.timer.killTimer()
+                self.showAlert = true }) {
+                Text("Hit Me!")
+                
+              }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(computeScore())))
+              }.padding()
+              ColorSlider(value: $rGuess, textColor: .red)
+              ColorSlider(value: $gGuess, textColor: .green)
+              ColorSlider(value: $bGuess, textColor: .blue)
+        }.padding(.horizontal)
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
+            //.environment(\.colorScheme, .dark)
         }
-        VStack {
-          Color(red: rGuess, green: gGuess, blue: bGuess)
-          Text("R: \(Int(rGuess * 255.0))"
-            + "  G: \(Int(gGuess * 255.0))"
-            + "  B: \(Int(bGuess * 255.0))")
-        }
-      }
-      Button(action: { self.showAlert = true }) {
-        Text("Hit Me!")
-      }.alert(isPresented: $showAlert) {
-        Alert(title: Text("Your Score"), message: Text(String(computeScore())))
-      }.padding()
-      ColorSlider(value: $rGuess, textColor: .red)
-      ColorSlider(value: $gGuess, textColor: .green)
-      ColorSlider(value: $bGuess, textColor: .blue)
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -66,7 +85,9 @@ struct ColorSlider: View {
     HStack {
       Text("0").foregroundColor(textColor)
       Slider(value: $value)
+        .background(textColor)
+        .cornerRadius(10)
       Text("255").foregroundColor(textColor)
-    }.padding(.horizontal)
+    }
   }
 }
---
+++ Color Match SwiftUI/TimeCounter.swift
@@ -0,0 +1,28 @@
+//
+//  TimeCounter.swift
+//  Color Match SwiftUI
+//
+//  Created by Johan on 14/06/2020.
+//  Copyright © 2020 Devscorch. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class TimeCounter: ObservableObject {
+    var timer: Timer?
+    @Published var counter = 0
+    
+    @objc func updateCounter() {
+        counter += 1
+    }
+    
+    init() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
